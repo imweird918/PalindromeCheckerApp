2d28acac91f1b2e45a0b6ea6d15b02175acd46ea
imweird918 <dhanushshaji06@gmail.com>
2026-03-10 23:59:17 +0530

    UC12:Strategy Pattern for Palindrome Algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,34 +1,34 @@
+
+import java.util.Stack;
+
+class StackStrategy implements PalindromeStrategy {
+    @Override
+    public boolean check(String input) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+interface PalindromeStrategy {
+    boolean check(String input);
+}
 public class PalindromeCheckerApp {
-
     public static void main(String[] args) {
-        String input = "racecar";
+        String input = "level";
 
-        // Create an instance of the service
-        PalindromeService service = new PalindromeService();
+        // We can choose any strategy that implements PalindromeStrategy
+        PalindromeStrategy strategy = new StackStrategy();
 
-        // Use the service to check the palindrome
-        boolean isPalindrome = service.checkPalindrome(input);
+        boolean isPalindrome = strategy.check(input);
 
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
-    }
-}
-
-class PalindromeService {
-    /**
-     * Checks whether the input string is a palindrome.
-     */
-    public boolean checkPalindrome(String input) {
-        int start = 0;
-        int end = input.length() - 1;
-
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
     }
 }
